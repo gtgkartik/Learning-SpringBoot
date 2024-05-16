737da853113ee0cde4dc69af514548ae29b3ee59
Kartik Kumar <gtgkartik@gmail.com>
2024-05-16 13:04:10 +0530

    some minor changes to vcs

--- learn-spring-framework-02/src/main/java/com/kartikkumar/learnspringframework/App3GamingSpringBeans/App3GamingSpringBeans.java
+++ learn-spring-framework-02/src/main/java/com/kartikkumar/learnspringframework/App3GamingSpringBeans/App3GamingSpringBeans.java
@@ -7,7 +7,7 @@ public class App3GamingSpringBeans {
 
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(GamingConfiguration.class);
-//        context.getBean(GamingConsole.class).up();
+//        context.getBean(GamingConsole.class).up()
         context.getBean(GameRunner.class).run();
     }
 }
