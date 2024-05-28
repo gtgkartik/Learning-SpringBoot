a20307f6d165d5063ee1aa495019a4dc389b6425
kartik kumar <gtgkartik@gmail.com>
2024-05-28 15:45:13 +0530

    learning new module

---
+++ learn-springboot-03/learingspringboot/src/main/java/com/learingspringboot/learingspringboot/LearingspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.learingspringboot.learingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearingspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearingspringbootApplication.class, args);
+	}
+
+}
---
+++ learn-springboot-03/learingspringboot/src/test/java/com/learingspringboot/learingspringboot/LearingspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learingspringboot.learingspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearingspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
